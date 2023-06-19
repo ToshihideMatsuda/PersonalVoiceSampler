a0082c564e4c9c07b46fe6f2b1974424c82b3f0c
mazdato <toshihide.matsuda@gmail.com>
2023-06-19 10:07:41 +0900

    initialize

---
+++ PersonalVoiceSampler/PersonalVoiceSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalVoiceSamplerApp.swift
+//  PersonalVoiceSampler
+//
+//  Created by tmatsuda on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct PersonalVoiceSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
